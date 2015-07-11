4d90948727ee86ab09a351971038bbf90945a4e3
moonwave99 <diego.caponera@gmail.com>
2015-07-11 14:33:04 +0200

    Update logging.

--- src/renderer/stores/OpenPlaylistStore.js
+++ src/renderer/stores/OpenPlaylistStore.js
@@ -20,8 +20,8 @@ var _activeIndex = -1
 var _selectPlaylist = function(playlist, index){
   _selectedIndex = index
   playa.playlistLoader.load(playlist).then((playlist)=>{
-    console.info('Selected ' + playlist.id)
-    OpenPlaylistStore.emitChange()  
+    console.info('Selected ' + playlist.id, playlist)
+    OpenPlaylistStore.emitChange()
   })
 }
 
@@ -37,23 +37,23 @@ var OpenPlaylistStore = assign({}, EventEmitter.prototype, {
   getAll: function(){
     return _playlists
   },
-  
+
   getAt: function(index){
     return _playlists[index]
   },
-  
+
   getSelectedIndex: function(){
     return _selectedIndex
   },
-  
+
   getSelectedPlaylist: function(){
     return _playlists[_selectedIndex]
-  },  
+  },
 
   getActiveIndex: function(){
     return _activeIndex
   },
-  
+
   emitChange: function(){
     this.emit(CHANGE_EVENT)
   },
@@ -71,7 +71,7 @@ var OpenPlaylistStore = assign({}, EventEmitter.prototype, {
   removeChangeListener: function(callback){
     this.removeListener(CHANGE_EVENT, callback)
   },
-  
+
   dispatcherIndex: AppDispatcher.register(function(action) {
     switch(action.actionType) {
       case OpenPlaylistConstants.SELECT_PLAYLIST:
@@ -86,7 +86,7 @@ var OpenPlaylistStore = assign({}, EventEmitter.prototype, {
         if(playlist){
           _selectPlaylist(playlist, index)
         }
-        break           
+        break
       case OpenPlaylistConstants.ADD_PLAYLIST:
         var newPlaylists = _.difference(action.playlists.map( i => i.id), _playlists.map(i => i.id))
         _playlists = _playlists.concat(action.playlists.filter((p)=>{
@@ -101,32 +101,32 @@ var OpenPlaylistStore = assign({}, EventEmitter.prototype, {
             console.info('Saved ' + playlist.id)
             OpenPlaylistStore.emitChange()
           })
-        }        
+        }
         break
       case OpenPlaylistConstants.ADD_FOLDER:
         var playlist = _getSelectedPlaylist()
         if(action.folder && playlist) {
           playlist.addFolder(action.folder).then(()=>{
-            OpenPlaylistStore.emitChange()  
+            OpenPlaylistStore.emitChange()
           }).catch((err)=>{
             console.error(err.stack)
-          })  
-        }        
+          })
+        }
         break
       case OpenPlaylistConstants.PLAY_ALBUM:
         var playlist = _playlists[action.playlist.id]
         if(action.playlist.id !== _activeIndex){
-          _activeIndex = action.playlist.id          
+          _activeIndex = action.playlist.id
         }
         playa.player.userPlaylist = action.playlist
         playa.player.playAlbum(action.album, action.trackId)
-        break        
+        break
       case OpenPlaylistConstants.REMOVE_FILES:
         var playlist = _getSelectedPlaylist()
         if(playlist){
           _getSelectedPlaylist().removeItems(action.ids)
         }
-        break        
+        break
       case OpenPlaylistConstants.CLOSE_PLAYLIST:
         var playlist = _getSelectedPlaylist()
         if(playlist){
@@ -136,11 +136,11 @@ var OpenPlaylistStore = assign({}, EventEmitter.prototype, {
           })
           var nexPlaylist = _getAt(_selectedIndex -1)
           if(nexPlaylist){
-            _selectPlaylist(nexPlaylist, _selectedIndex-1)  
+            _selectPlaylist(nexPlaylist, _selectedIndex-1)
           }else{
             OpenPlaylistStore.emitChange()
           }
-          
+
         }
         break
       case OpenPlaylistConstants.REORDER_PLAYLIST:
@@ -148,13 +148,13 @@ var OpenPlaylistStore = assign({}, EventEmitter.prototype, {
         if(playlist){
           playlist.reorder(action.from, action.to)
           OpenPlaylistStore.emitChange()
-        }        
+        }
         break
     }
 
     return true // No errors. Needed by promise in Dispatcher.
-  })  
-    
+  })
+
 })
 
 module.exports = OpenPlaylistStore
