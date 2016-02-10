947b48f773fd247d7e9f6c57bc8645a74e6bf223
moonwave99 <diego.caponera@gmail.com>
2016-02-10 23:30:01 +0100

    Sanitise PlaylistItem values.

--- src/renderer/util/PlaylistItem.js
+++ src/renderer/util/PlaylistItem.js
@@ -1,12 +1,16 @@
 "use babel"
 
-var md5 = require('MD5')
+var md5 = require('md5')
 
 module.exports = class PlaylistItem{
   constructor(options){
-    this.metadata = options.metadata
-    this.duration = options.duration
+    this.metadata = options.metadata || {}
+    this.duration = +options.duration
     this.filename = options.filename
     this.id = 't_' + md5(this.filename)
+    this.disabled = options.disabled
+  }
+  getDiscNumber(){
+    return this.disabled ? 0 : this.metadata.disk.no
   }
 }
