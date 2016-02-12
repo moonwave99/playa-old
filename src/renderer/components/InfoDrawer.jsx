9ff94872a449f50fb4f5dbafe84be023bb2a53e3
moonwave99 <diego.caponera@gmail.com>
2016-02-12 16:13:44 +0100

    Display current track info in InfoDrawer.

--- src/renderer/components/InfoDrawer.jsx
+++ src/renderer/components/InfoDrawer.jsx
@@ -8,14 +8,42 @@ var ReactPropTypes = React.PropTypes
 var cx = require('classnames')
 
 var OpenPlaylistActions = require('../actions/OpenPlaylistActions')
+var AudioMetadata = require('../util/AudioMetadata')
 
 var InfoDrawer = React.createClass({
+  componentWillReceiveProps: function(nextProps){
+    let currentTrack = playa.player.currentTrack
+    if(!currentTrack){
+      return
+    }
+    let metadata = new AudioMetadata(currentTrack.filename)
+    metadata.load().then(()=>{
+      this.setState({
+        audioInfo: metadata.toJSON()
+      })
+    })
+  },
+  getInitialState: function(){
+    return {
+      audioInfo: null
+    }
+  },
+  renderCurrentTrackInfo: function(){
+    return (
+      <ul className="list-unstyled track-info">
+        { _.map(this.state.audioInfo, (value, key)=>{
+          return <li key={key}><span className="key">{key.replace('_', ' ')}:</span><span className="value">{value}</span></li>
+        })}
+      </ul>
+    )
+  },
   render: function() {
-    var classes = cx({
+    let info = playa.player.playbackInfo()
+    let classes = cx({
       'info-drawer' : true
     })
     return (
-      <div className={classes}></div>
+      <div className={classes}><h3>Now Playing</h3>{ this.state.audioInfo ? this.renderCurrentTrackInfo() : <p>Seems like you are enjoying the silence now.</p> }</div>
     )
   }
 })
