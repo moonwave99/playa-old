eb279de56ebc6720ace895d4010dd6ebc2c93849
moonwave99 <diego.caponera@gmail.com>
2016-02-17 17:02:05 +0100

    Rearrange remote controller layout.

--- src/ui/riot-tags/remote.js
+++ src/ui/riot-tags/remote.js
@@ -1,5 +1,48 @@
 <remote class="{ remote-controller: true, show-album: this.showAlbum }">
 
+  <div class="panel-wrapper">
+    <section class="panel playlist">
+      <ul class="list-unstyled">
+        <li
+          each={ album, a in playlist.albums }
+          class={ clearfix: true, disabled: album.disabled, playing: album.id == parent.currentAlbum.id }
+          onclick={parent.onAlbumClick.bind(parent)} data-id={album.id}>
+          <span class="cover" onclick={parent.onCoverClick.bind(parent)} data-action="gotoAlbum">
+            <img src={ '/covers/' + album.cover }>
+          </span>
+          <span class="artist">{ album.artist }</span>
+          <span class="title">{ album.title } <i class="fa fa-fw fa-volume-up playing-icon"></i></span>
+          <span class="year">{ album.year }</span>
+        </li>
+      </ul>
+    </section>
+
+    <section class="panel album-detail">
+      <header>
+        <h2>
+          <span class="artist">{ this.shownAlbum.artist }</span>
+          <span class="title">{ this.shownAlbum.title }</span>
+          <span class="year">{ this.shownAlbum.year }</span>
+          <img class="cover" src={ '/covers/' + this.shownAlbum.cover }>
+        </h2>
+      </header>
+      <ol class="list-unstyled">
+        <li
+          each={ track, t in this.shownAlbum.tracks }
+          class={playing: track.id == parent.playbackInfo.currentTrackID}
+          onclick={parent.onTrackClick.bind(parent)}
+          data-id={track.id}
+          data-album-id={parent.shownAlbum.id}
+          data-action="gotoTrack">
+          <i class="fa fa-fw fa-volume-up playing-icon"></i>
+          <span class="track-number">{ track.track }</span>
+          <span class="track-title">{ track.title }</span>
+          <span class="track-duration">{ track.formattedDuration }</span>
+        </li>
+      </ol>
+    </section>
+  </div>
+
   <section class="{ playback-bar: true, playing: this.playbackInfo.playing }">
     <div class="buttons">
       <button onClick={this.onPlaybackBarClick.bind(this)} data-action="prev">
@@ -20,51 +63,12 @@
       data-action="seekTo">
     </progress>
     <p class="playback-info">{ this.currentTrack.artist ? (this.currentTrack.artist + ' - ' + this.currentTrack.title) : '' }</p>
-  </section>
-
-  <section class="panel playlist">
-    <h2 class="header">
+    <div class="footer playlist-title">
       <i class="fa fa-fw fa-file-audio-o"></i> { this.playlist.title }
-    </h2>
-    <ul class="list-unstyled">
-      <li
-        each={ album, a in albums }
-        class={ clearfix: true, disabled: album.disabled, playing: album.id == parent.playbackInfo.currentAlbumID }
-        onclick={parent.onAlbumClick.bind(parent)} data-id={album.id}>
-        <span class="cover" onclick={parent.onCoverClick.bind(parent)} data-action="gotoAlbum">
-          <img src={ '/covers/' + album.cover }>
-        </span>
-        <span class="artist">{ album.artist }</span>
-        <span class="title">{ album.title }</span>
-        <span class="year">{ album.year }</span>
-      </li>
-    </ul>
-  </section>
-
-  <section class="panel album-detail">
-    <a href="#" class="header" onclick={this.backToAlbumList.bind(this)}>
+    </div>
+    <a href="#" class="footer back-button" onclick={this.backToAlbumList.bind(this)}>
       <i class="fa fa-fw fa-chevron-left"></i> Back to Album List
     </a>
-    <header>
-      <h2>
-        <span class="artist">{ this.shownAlbum.artist }</span>
-        <span class="title">{ this.shownAlbum.title }</span>
-        <span class="year">{ this.shownAlbum.year }</span>
-        <img class="cover" src={ '/covers/' + this.shownAlbum.cover }>
-      </h2>
-    </header>
-    <ol class="list-unstyled">
-      <li
-        each={ track, t in this.shownAlbum.tracks }
-        onclick={parent.onTrackClick.bind(parent)}
-        data-id={track.id}
-        data-album-id={parent.shownAlbum.id}
-        data-action="gotoTrack">
-        <span class="track-number">{ track.track }</span>
-        <span class="track-title">{ track.title }</span>
-        <span class="track-duration">{ track.formattedDuration }</span>
-      </li>
-    </ol>
   </section>
 
   <script>
