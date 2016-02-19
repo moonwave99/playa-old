2cd507bba6c4fc1540c2a3a9ccdcd289c3b0eadc
moonwave99 <diego.caponera@gmail.com>
2016-02-19 01:27:31 +0100

    Remove remainders from atom-shell era.

--- script/utils/config.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-var pkg = require('../../package.json');
-
-if (process.platform === 'win32') {
-  process.env.ATOM_HOME = path.join(process.env.USERPROFILE, '.' + pkg.name);
-} else {
-  process.env.ATOM_HOME = path.join(process.env.HOME, '.' + pkg.name);
-}
