6ad3a0e4196076b3fe554afb553f6fc0017485a6
Diego Caponera <diego.caponera@gmail.com>
2017-01-26 12:54:42 +0100

    (chore) lint and es6 generators.

--- src/renderer/generators/Navigable.jsx
+++ src/renderer/generators/Navigable.jsx
@@ -1,250 +1,284 @@
-"use babel"
+'use babel';
 
-var _ = require('lodash')
-var React = require('react')
-var ReactPropTypes = React.PropTypes
+import { difference, without, uniq } from 'lodash';
+import React, { Component as ReactComponent, PropTypes } from 'react';
+import KeyboardFocusStore from '../stores/KeyboardFocusStore';
+import KeyboardFocusActions from '../actions/KeyboardFocusActions';
+import NavigableConstants from '../constants/NavigableConstants';
 
-var KeyboardFocusStore = require('../stores/KeyboardFocusStore')
-var KeyboardFocusActions = require('../actions/KeyboardFocusActions')
+const _alphanumericRegex = new RegExp('^[a-zA-Z0-9\\s]+$');
+const BUFFER_RESET_TIMEOUT = 500;
+const SIMPLE_SELECTION_HANDLERS = 'up, down, alt+up, alt+down';
+const MULTIPLE_SELECTION_HANDLERS = 'up, down, shift+up, shift+down, alt+up, alt+down, shift+alt+up, shift+alt+down';
 
-var NavigableConstants = require('../constants/NavigableConstants')
+const defaultGetSelectedIds = function defaultGetSelectedIds(component) {
+  return component.state.selection;
+};
 
-var _alphanumericRegex = new RegExp("^[a-zA-Z0-9\\s]+$")
+const defaultSearchForBuffer = function defaultSearchForBuffer() {};
 
-module.exports = function(Component, scopeName, getIdList, getSelectedElement, getSelectedIds, searchForBuffer){
-  getSelectedIds = getSelectedIds || function(component){
-    return component.state.selection
-  }
-  searchForBuffer = searchForBuffer || function(component, buffer){}
-
-  const NavigableComponent = React.createClass({
-    propTypes: {
-      handleDelKeyPress: ReactPropTypes.func.isRequired,
-      handleEnterKeyPress: ReactPropTypes.func.isRequired,
-      handleScrollToElement: ReactPropTypes.func.isRequired,
-      handleOpen: ReactPropTypes.func,
-      handleClose: ReactPropTypes.func,
-      isFocused: ReactPropTypes.bool,
-      allowMultipleSelection: ReactPropTypes.bool
-    },
-    getIdList() {
-      return getIdList(this)
-    },
-    getSelectedElement(){
-      return getSelectedElement(this)
-    },
-    getSelectedIds(){
-      return getSelectedIds(this)
-    },
-    searchForBuffer(buffer){
-      return searchForBuffer(this, buffer)
-    },
-    getInitialState() {
-      return {
+module.exports = function Navigable(
+  Component,
+  scopeName,
+  getIdList,
+  getSelectedElement,
+  getSelectedIds = defaultGetSelectedIds,
+  searchForBuffer = defaultSearchForBuffer,
+) {
+  class NavigableComponent extends ReactComponent {
+    constructor(props) {
+      super(props);
+      this.state = {
         selection: this.props.initSelection || [],
         openElements: this.props.initOpenElements || [],
-        direction: 0
-      }
-    },
+        direction: 0,
+      };
+      this._onKeyboardFocusChange = this._onKeyboardFocusChange.bind(this);
+      this._clearBufferTimeout = this._clearBufferTimeout.bind(this);
+      this._scheduleBufferReset = this._scheduleBufferReset.bind(this);
+      this.getIdList = this.getIdList.bind(this);
+      this.handleClick = this.handleClick.bind(this);
+      this.handleEnterKeyPress = this.handleEnterKeyPress.bind(this);
+      this.handleArrowKeyPress = this.handleArrowKeyPress.bind(this);
+      this.handleLeftRightKeyPress = this.handleLeftRightKeyPress.bind(this);
+      this.handleDelKeyPress = this.handleDelKeyPress.bind(this);
+      this.handleSelectAllKeyPress = this.handleSelectAllKeyPress.bind(this);
+      this.handleTextKeyPress = this.handleTextKeyPress.bind(this);
+    }
     componentDidMount() {
-      KeyboardFocusStore.addChangeListener(this._onKeyboardFocusChange)
-      this._keyBuffer = ''
-      this._resetBufferTimeout = null
-    },
-    componentWillUnmount() {
-      KeyboardFocusStore.removeChangeListener(this._onKeyboardFocusChange)
-      this._clearBufferTimeout()
-    },
-    componentDidUpdate(prevProps, prevState){
-      this.props.handleScrollToElement(this.state, this.getIdList(), this.refs.component)
-      if(this.props.isFocused){
-        this.onFocusRequest()
+      KeyboardFocusStore.addChangeListener(this._onKeyboardFocusChange);
+      this._keyBuffer = '';
+      this._resetBufferTimeout = null;
+    }
+    componentDidUpdate() {
+      this.props.handleScrollToElement(this.state, this.getIdList(), this.component);
+      if (this.props.isFocused) {
+        this.onFocusRequest();
       }
-    },
+    }
+    componentWillUnmount() {
+      KeyboardFocusStore.removeChangeListener(this._onKeyboardFocusChange);
+      this._clearBufferTimeout();
+    }
+    onFocusRequest(params = {}) {
+      if (params.id && params.direction) {
+        const ids = this.getIdList();
+        const currentIndex = ids.indexOf(params.id);
+        if (currentIndex < ids.length - 1 && currentIndex > -1) {
+          this.setState({
+            selection: [ids[currentIndex + (params.direction === 'up' ? 0 : 1)]],
+          });
+        }
+      }
+      if (params.requestFocus) {
+        KeyboardFocusActions.setFocus(this.getHandlers(), scopeName);
+      }
+    }
+    getIdList() {
+      return getIdList(this);
+    }
+    getSelectedElement() {
+      return getSelectedElement(this);
+    }
+    getSelectedIds() {
+      return getSelectedIds(this);
+    }
+    getHandlers() {
+      const handlers = {
+        'backspace, del': this.handleDelKeyPress,
+        enter: this.handleEnterKeyPress,
+        'command+a': this.handleSelectAllKeyPress,
+        'left, right': this.handleLeftRightKeyPress,
+        '*': this.handleTextKeyPress,
+      };
+      if (this.props.allowMultipleSelection) {
+        handlers[MULTIPLE_SELECTION_HANDLERS] = this.handleArrowKeyPress;
+      } else {
+        handlers[SIMPLE_SELECTION_HANDLERS] = this.handleArrowKeyPress;
+      }
+      return handlers;
+    }
+    handleClick(event, item) {
+      const ids = this.getIdList();
+      const index = ids.indexOf(item.props.itemKey);
+      const [low, hi] = [
+        ids.indexOf(this.state.selection[0]),
+        ids.indexOf(this.state.selection[this.state.selection.length - 1]),
+      ];
+
+      if (event.metaKey) {
+        this.setState({
+          selection: item.props.isSelected
+            ? without(this.state.selection, item.props.itemKey)
+            : this.state.selection.concat([item.props.itemKey]),
+        });
+      } else if (event.shiftKey && this.props.allowMultipleSelection) {
+        this.setState({
+          selection: ids.slice(
+            Math.min(low, index), Math.max(hi, index) + 1
+          ),
+        });
+      } else {
+        this.setState({
+          selection: [item.props.itemKey],
+          lastAction: NavigableConstants.MOUSE_INPUT,
+        });
+      }
+    }
+    handleEnterKeyPress(event) {
+      this.props.handleEnterKeyPress(event, this);
+    }
+    handleArrowKeyPress(event) {
+      const ids = this.getIdList();
+      const [low, hi] = [
+        ids.indexOf(this.state.selection[0]),
+        ids.indexOf(this.state.selection[this.state.selection.length - 1]),
+      ];
+      let newLow = low;
+      let newHi = hi;
+      let direction = 0;
+      switch (event.which) {
+        case 38: // up
+          direction = -1;
+          if (event.shiftKey && event.altKey) {
+            newLow = 0;
+          } else if (event.shiftKey) {
+            newLow = Math.max(0, low - 1);
+          } else if (event.altKey) {
+            newLow = newHi = 0;
+          } else {
+            newLow = Math.max(0, low - 1);
+            newHi = newLow;
+          }
+          break;
+        case 40: // down
+          direction = 1;
+          if (event.shiftKey && event.altKey) {
+            newHi = ids.length - 1;
+          } else if (event.shiftKey) {
+            newHi = Math.min(ids.length - 1, hi + 1);
+          } else if (event.altKey) {
+            newLow = newHi = ids.length - 1;
+          } else {
+            newLow = Math.min(ids.length - 1, low + 1);
+            newHi = newLow;
+          }
+          break;
+        default:
+          break;
+      }
+      this.setState({
+        selection: ids.slice(newLow, newHi + 1),
+        direction,
+        lastAction: NavigableConstants.KEYBOARD_INPUT,
+      });
+    }
+    handleLeftRightKeyPress(event) {
+      switch (event.which) {
+        case 39: // right
+          this.openElements(this.state.selection);
+          break;
+        case 37: // left
+          this.closeElements(this.state.selection);
+          break;
+        default:
+          break;
+      }
+    }
+    handleDelKeyPress(event) {
+      this.props.handleDelKeyPress(event, this, getSelectedIds(this));
+      this.setState({
+        selection: [],
+        lastAction: null,
+      });
+    }
+    handleSelectAllKeyPress() {
+      this.setState({
+        selection: this.getIdList(),
+        lastAction: NavigableConstants.KEYBOARD_INPUT,
+      });
+    }
+    handleTextKeyPress(event) {
+      const pressedKey = String.fromCharCode(event.which);
+      if (!_alphanumericRegex.test(pressedKey)) {
+        return;
+      }
+      this._clearBufferTimeout();
+      this._keyBuffer += pressedKey.toLowerCase();
+      this.gotoItemForBuffer(this._keyBuffer);
+      this._scheduleBufferReset();
+    }
+    gotoItemForBuffer(buffer) {
+      const result = this.searchForBuffer(buffer);
+      if (result) {
+        this.setState({
+          selection: [result],
+        });
+      }
+    }
+    openElements(ids) {
+      this.setState({
+        openElements: uniq(this.state.openElements.concat(ids)),
+        lastAction: NavigableConstants.KEYBOARD_INPUT,
+      });
+      if (this.props.handleOpen) {
+        this.props.handleOpen(ids);
+      }
+    }
+    closeElements(ids) {
+      this.setState({
+        openElements: difference(this.state.openElements, ids),
+        lastAction: NavigableConstants.KEYBOARD_INPUT,
+      });
+      if (this.props.handleClose) {
+        this.props.handleClose(ids);
+      }
+    }
+    searchForBuffer(buffer) {
+      return searchForBuffer(this, buffer);
+    }
+    _onKeyboardFocusChange() {
+      const currentScopeName = KeyboardFocusStore.getCurrentScopeName();
+      if (scopeName === currentScopeName) {
+        this._keyBuffer = '';
+        KeyboardFocusActions.setFocus(this.getHandlers(), scopeName);
+      }
+    }
+    _clearBufferTimeout() {
+      if (this._resetBufferTimeout) {
+        clearTimeout(this._resetBufferTimeout);
+      }
+    }
+    _scheduleBufferReset() {
+      this._resetBufferTimeout = setTimeout(
+        () => { this._keyBuffer = ''; },
+        BUFFER_RESET_TIMEOUT,
+      );
+    }
     render() {
       return (
         <Component
-          ref="component"
+          ref={(c) => { this.component = c; }}
           handleClick={this.handleClick}
           focusParent={this.onFocusRequest}
           closeElements={this.closeElements}
           {...this.props}
-          {...this.state} />
-      )
-    },
-    onFocusRequest(params = {}){
-      if(params.id && params.direction){
-        var ids = this.getIdList()
-        var currentIndex = ids.indexOf(params.id)
-        if(currentIndex < ids.length -1 && currentIndex > -1){
-          this.setState({
-            selection : [ids[currentIndex + (params.direction == 'up' ? 0 : 1)]]
-          })
-        }
-      }
-      if(params.requestFocus){
-        KeyboardFocusActions.setFocus(this.getHandlers(), scopeName)
-      }
-    },
-    handleClick(event, item) {
-      var ids = this.getIdList()
-      var index = ids.indexOf(item.props.itemKey)
-      var [low, hi] = [
-        ids.indexOf(this.state.selection[0]),
-        ids.indexOf(this.state.selection[this.state.selection.length-1])
-      ]
-
-      if(event.metaKey){
-        this.setState({
-          selection: item.props.isSelected ? _.without(this.state.selection, item.props.itemKey) : this.state.selection.concat([item.props.itemKey])
-        })
-      }else if(event.shiftKey && this.props.allowMultipleSelection){
-        this.setState({
-          selection: ids.slice(
-            Math.min(low, index), Math.max(hi, index)+1
-          )
-        })
-      }else{
-        this.setState({
-          selection: [item.props.itemKey],
-          lastAction: NavigableConstants.MOUSE_INPUT
-        })
-      }
-    },
-    handleEnterKeyPress(event) {
-      this.props.handleEnterKeyPress(event, this)
-    },
-    handleArrowKeyPress(event) {
-      var ids = this.getIdList()
-      var [low, hi] = [
-        ids.indexOf(this.state.selection[0]),
-        ids.indexOf(this.state.selection[this.state.selection.length-1])
-      ]
-      var newLow = low
-      var newHi = hi
-      var direction = 0
-      switch(event.which){
-        case 38: // up
-          direction = -1
-          if(event.shiftKey && event.altKey){
-            newLow = 0
-          }else if(event.shiftKey){
-            newLow = Math.max(0, low-1)
-          }else if(event.altKey){
-            newLow = newHi = 0
-          }else{
-            newLow = Math.max(0, low-1)
-            newHi = newLow
-          }
-          break
-        case 40: // down
-          direction = 1
-          if(event.shiftKey && event.altKey){
-            newHi = ids.length-1
-          }else if(event.shiftKey){
-            newHi = Math.min(ids.length-1, hi+1)
-          }else if(event.altKey){
-            newLow = newHi = ids.length-1
-          }else{
-            newLow = Math.min(ids.length-1, low+1)
-            newHi = newLow
-          }
-          break
-      }
-      this.setState({
-        selection: ids.slice(newLow, newHi+1),
-        direction: direction,
-        lastAction: NavigableConstants.KEYBOARD_INPUT
-      })
-    },
-    handleLeftRightKeyPress(event){
-      switch(event.which){
-        case 39: // right
-          this.openElements(this.state.selection)
-          break
-        case 37: // left
-          this.closeElements(this.state.selection)
-          break
-      }
-    },
-    handleDelKeyPress(event) {
-      this.props.handleDelKeyPress(event, this, getSelectedIds(this))
-      this.setState({
-        selection: [],
-        lastAction: null
-      })
-    },
-    handleSelectAllKeyPress(event) {
-      this.setState({
-        selection: this.getIdList(),
-        lastAction: NavigableConstants.KEYBOARD_INPUT
-      })
-    },
-    handleTextKeyPress: function(event){
-      let pressedKey = String.fromCharCode(event.which)
-      if(!_alphanumericRegex.test(pressedKey)){
-        return
-      }
-      this._clearBufferTimeout();
-
-      this._keyBuffer += pressedKey.toLowerCase()
-      this.gotoItemForBuffer(this._keyBuffer)
-      this._scheduleBufferReset()
-    },
-    gotoItemForBuffer(buffer){
-      let result = this.searchForBuffer(buffer)
-      if(!result){
-        return
-      }
-      this.setState({
-        selection: [result]
-      })
-    },
-    openElements(ids){
-      this.setState({
-        openElements: _.uniq(this.state.openElements.concat(ids)),
-        lastAction: NavigableConstants.KEYBOARD_INPUT
-      })
-      this.props.handleOpen && this.props.handleOpen(ids)
-    },
-    closeElements(ids){
-      this.setState({
-        openElements: _.difference(this.state.openElements, ids),
-        lastAction: NavigableConstants.KEYBOARD_INPUT
-      })
-      this.props.handleClose && this.props.handleClose(ids)
-    },
-    getHandlers(){
-      var handlers = {
-        'backspace, del'  : this.handleDelKeyPress,
-        'enter'           : this.handleEnterKeyPress,
-        'command+a'       : this.handleSelectAllKeyPress,
-        'left, right'     : this.handleLeftRightKeyPress,
-        '*'               : this.handleTextKeyPress
-      }
-      if(this.props.allowMultipleSelection){
-        handlers['up, down, shift+up, shift+down, alt+up, alt+down, shift+alt+up, shift+alt+down'] = this.handleArrowKeyPress
-      }else{
-        handlers['up, down, alt+up, alt+down'] = this.handleArrowKeyPress
-      }
-      return handlers
-    },
-    _onKeyboardFocusChange: function(){
-      var currentScopeName = KeyboardFocusStore.getCurrentScopeName()
-      if(scopeName == currentScopeName){
-        this._keyBuffer = ''
-        KeyboardFocusActions.setFocus(this.getHandlers(), scopeName)
-      }
-    },
-    _clearBufferTimeout: function(){
-      if(this._resetBufferTimeout){
-        clearTimeout(this._resetBufferTimeout)
-      }
-    },
-    _scheduleBufferReset: function(){
-      this._resetBufferTimeout = setTimeout( ()=> { this._keyBuffer = '' }, 500)
+          {...this.state}
+        />
+      );
     }
-  })
+  }
 
-  return NavigableComponent
-}
+  NavigableComponent.propTypes = {
+    handleDelKeyPress: PropTypes.func.isRequired,
+    handleEnterKeyPress: PropTypes.func.isRequired,
+    handleScrollToElement: PropTypes.func.isRequired,
+    handleOpen: PropTypes.func,
+    handleClose: PropTypes.func,
+    isFocused: PropTypes.bool,
+    allowMultipleSelection: PropTypes.bool,
+    initOpenElements: PropTypes.arrayOf(PropTypes.string),
+    initSelection: PropTypes.arrayOf(PropTypes.string),
+  };
+  return NavigableComponent;
+};
