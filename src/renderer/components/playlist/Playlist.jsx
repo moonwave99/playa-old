ac49e1ad261895cd767f01de62238b90f2bcf33c
Diego Caponera <diego.caponera@gmail.com>
2017-01-26 17:01:19 +0100

    (chore) lint and es6 playlist components.

--- src/renderer/components/playlist/Playlist.jsx
+++ src/renderer/components/playlist/Playlist.jsx
@@ -1,115 +1,137 @@
-"use babel"
+'use babel';
 
-var _ = require('lodash')
-var cx = require('classnames')
-var React = require('react')
-var ReactPropTypes = React.PropTypes
-var AlbumPlaylist = require('./AlbumPlaylist.jsx')
+import { contains, reduce } from 'lodash';
+import React, { PropTypes, Component } from 'react';
+import cx from 'classnames';
+import AlbumPlaylist from './AlbumPlaylist.jsx';
+import OpenPlaylistActions from '../../actions/OpenPlaylistActions';
+import PlayerStore from '../../stores/PlayerStore';
+import KeyboardFocusActions from '../../actions/KeyboardFocusActions';
+import navGenerator from '../../generators/Navigable.jsx';
+import KeyboardNameSpaceConstants from '../../constants/KeyboardNameSpaceConstants';
 
-var OpenPlaylistActions = require('../../actions/OpenPlaylistActions')
-var PlayerActions = require('../../actions/PlayerActions')
-var PlayerStore = require('../../stores/PlayerStore')
-var NavGenerator = require('../../generators/Navigable.jsx')
-
-var KeyboardFocusActions = require('../../actions/KeyboardFocusActions')
-var KeyboardFocusConstants = require('../../constants/KeyboardFocusConstants')
-var KeyboardNameSpaceConstants = require('../../constants/KeyboardNameSpaceConstants')
-
-var AlbumPlaylistOnSteroids = NavGenerator(AlbumPlaylist, KeyboardNameSpaceConstants.ALBUM_PLAYLIST,
-  function(component){
-    var ids = _.reduce(component.props.playlist.getItems(), (memo, album)=>{
-      memo.push(album.id)
-      if(_.contains(component.state.openElements, album.id)){
-        memo = memo.concat(album.tracks.map( t => t.id ))
+const AlbumPlaylistOnSteroids = navGenerator(
+  AlbumPlaylist,
+  KeyboardNameSpaceConstants.ALBUM_PLAYLIST,
+  component =>
+    reduce(component.props.playlist.getItems(), (memo, album) => {
+      let ids = [];
+      ids.push(album.id);
+      if (contains(component.state.openElements, album.id)) {
+        ids = ids.concat(album.tracks.map(t => t.id));
       }
-      return memo;
-    }, []);
-    return ids
-  },
-  function(component){
-    if(component.state.selection[0].startsWith('a_')){
-      var album = component.props.playlist.getAlbumById(component.state.selection[0])
+      return memo.concat(ids);
+    }, [])
+  ,
+  (component) => {
+    if (component.state.selection[0].startsWith('a_')) {
+      const album = component.props.playlist.getAlbumById(component.state.selection[0]);
       return {
-        album: album,
-        trackId: album.tracks[0].id
-      }
-    }else{
-      return {
-        album: component.props.playlist.getAlbumByTrackId(component.state.selection[0]),
-        trackId: component.state.selection[0]
-      }
+        album,
+        trackId: album.tracks[0].id,
+      };
     }
+    return {
+      album: component.props.playlist.getAlbumByTrackId(component.state.selection[0]),
+      trackId: component.state.selection[0],
+    };
   },
   null,
-  function(component, buffer){
-    let result = component.props.playlist.find(buffer) || {}
-    return result.id
-  }
-)
+  (component, buffer) => {
+    const result = component.props.playlist.find(buffer) || {};
+    return result.id;
+  },
+);
 
-function getPlayerState(){
-  var playerState = PlayerStore.getPlaybackInfo()
+const getPlayerState = function getPlayerState() {
+  const playerState = PlayerStore.getPlaybackInfo();
   return {
-    currentTrack: playerState.currentTrack || {}
-  }
-}
+    currentTrack: playerState.currentTrack || {},
+  };
+};
 
-var Playlist = React.createClass({
-  getInitialState: function(){
-    return {
-      currentTrack: {}
+const handleDelKeyPress = function handleDelKeyPress(event, item, tracksToRemove) {
+  OpenPlaylistActions.removeFiles(tracksToRemove, item.props.playlist);
+};
+const handleGlobalClick = function handleGlobalClick() {
+  KeyboardFocusActions.requestFocus(KeyboardNameSpaceConstants.ALBUM_PLAYLIST);
+};
+
+const handleScrollToElement = function handleScrollToElement(state, list, component) {
+  if (!state.selection[0]) {
+    return;
+  }
+  component.scrollAround(state.selection[0]);
+};
+
+class Playlist extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTrack: {},
+    };
+    this.handleEnterKeyPress = this.handleEnterKeyPress.bind(this);
+    this._onPlayerChange = this._onPlayerChange.bind(this);
+  }
+  componentDidMount() {
+    PlayerStore.addChangeListener(this._onPlayerChange);
+    if (!this.props.playlist.loaded) {
+      OpenPlaylistActions.load(this.props.playlist.id);
     }
-  },
-  componentDidMount: function(){
-    PlayerStore.addChangeListener(this._onPlayerChange)
-    if(!this.props.playlist.loaded){
-      OpenPlaylistActions.load(this.props.playlist.id)
+  }
+  componentWillUnmount() {
+    PlayerStore.removeChangeListener(this._onPlayerChange);
+  }
+  handleEnterKeyPress(event, item) {
+    if (item.state.selection.length !== 1) {
+      return;
     }
-  },
-  componentWillUnmount: function(){
-    PlayerStore.removeChangeListener(this._onPlayerChange)
-  },
-  render: function() {
-    var classes = cx({
-      'playlist'      : true,
-      'sidebar-open'  : !!this.props.isSidebarOpen
-    })
+    const whatToPlay = item.getSelectedElement();
+    OpenPlaylistActions.selectAlbum(
+      whatToPlay.album,
+      whatToPlay.trackId,
+      this.props.playlist,
+      true,
+    );
+  }
+  _onPlayerChange() {
+    const playerState = getPlayerState();
+    if (playerState.currentTrack.id !== this.state.currentTrack.id) {
+      this.setState(playerState);
+    }
+  }
+  render() {
+    const classes = cx({
+      playlist: true,
+      'sidebar-open': !!this.props.isSidebarOpen,
+    });
     return (
-      <div className={classes} onClick={this.handleGlobalClick}>
+      <div className={classes} onClick={handleGlobalClick}>
         <AlbumPlaylistOnSteroids
-          allowMultipleSelection={true}
+          allowMultipleSelection
           currentTrack={this.state.currentTrack}
           playlist={this.props.playlist}
           baseFontSize={this.props.baseFontSize}
           initSelection={[this.props.playlist.lastScrolledAlbumId]}
           initOpenElements={this.props.playlist.openAlbums}
-          handleDelKeyPress={this.handleDelKeyPress}
+          handleDelKeyPress={handleDelKeyPress}
           handleEnterKeyPress={this.handleEnterKeyPress}
-          handleScrollToElement={this.handleScrollToElement}/>
+          handleScrollToElement={handleScrollToElement}
+        />
       </div>
-    )
-  },
-  handleGlobalClick: function(event){
-    KeyboardFocusActions.requestFocus(KeyboardNameSpaceConstants.ALBUM_PLAYLIST)
-  },
-  handleDelKeyPress: function(event, item, tracksToRemove){
-    OpenPlaylistActions.removeFiles(tracksToRemove, item.props.playlist)
-  },
-  handleEnterKeyPress: function(event, item){
-    if(item.state.selection.length == 1){
-      var whatToPlay = item.getSelectedElement()
-      OpenPlaylistActions.selectAlbum(whatToPlay.album, whatToPlay.trackId, this.props.playlist, true)
-    }
-  },
-  handleScrollToElement: function(state, list, component){
-    state.selection[0] && component.scrollAround(state.selection[0])
-  },
-  _onPlayerChange: function(){
-    var playerState = getPlayerState()
-    if(playerState.currentTrack.id != this.state.currentTrack.id){
-      this.setState(playerState)
-    }
+    );
   }
-})
+}
 
-module.exports = Playlist
+Playlist.propTypes = {
+  isSidebarOpen: PropTypes.bool,
+  baseFontSize: PropTypes.number,
+  playlist: PropTypes.shape({
+    id: PropTypes.string,
+    loaded: PropTypes.bool,
+    lastScrolledAlbumId: PropTypes.string,
+    openAlbums: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
+
+module.exports = Playlist;
